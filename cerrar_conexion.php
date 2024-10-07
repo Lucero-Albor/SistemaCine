7cb2a323c1803a85a6dfa3787dfbd36812096c79
Lucero-Albor <lucerito856@gmail.com>
2024-10-07 10:17:40 -0600

    Conexion base de datos

---
+++ cerrar_conexion.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = '';
+$dbname = "thezine";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Cerrar conexión
+$conn->close();
+?>
